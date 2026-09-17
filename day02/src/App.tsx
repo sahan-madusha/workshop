9d90fb4ede3031845376ef9c5f875507359d422f
sahan madusha <sahanmadusha001@gmail.com>
2026-09-17 11:53:50 +0530

    feat: add initial application pages and navigation components

--- day02/src/App.tsx
+++ day02/src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
+import { Contact } from './pages/contact';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-slate-50 text-gray-900 flex flex-col justify-between font-sans">
-      <Header />
-      <main className="flex-grow">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-slate-50 text-gray-900 flex flex-col justify-between font-sans">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
